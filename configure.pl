1a0bfdd3105502c2cc715edebc8215023942312d
Hou <jh7x3@multicom.eecs.missouri.edu>
2019-05-26 21:12:15 -0500

    Update multicom1.0

--- configure.pl
+++ configure.pl
@@ -420,7 +420,7 @@ close PRCLIB;
 ### install boost-1.55 
 open(OUT,">$install_dir/installation/MULTICOM_manually_install_files/P1_install_boost.sh") || die "Failed to open file $install_dir/installation/MULTICOM_manually_install_files/P1_install_boost.sh\n";
 print OUT "#!/bin/bash -e\n\n";
-print OUT "echo \" Start compile boost (will take ~20 min)\n\"\n\n";
+print OUT "echo \" Start compile boost (will take ~20 min)\"\n\n";
 print OUT "cd $multicom_db_tools_dir/tools\n\n";
 print OUT "cd boost_1_55_0\n\n";
 print OUT "./bootstrap.sh  --prefix=$multicom_db_tools_dir/tools/boost_1_55_0\n\n";
@@ -431,7 +431,7 @@ close OUT;
 #### install OpenBlas
 open(OUT,">$install_dir/installation/MULTICOM_manually_install_files/P2_install_OpenBlas.sh") || die "Failed to open file $install_dir/installation/MULTICOM_manually_install_files/P2_install_OpenBlas.sh\n";
 print OUT "#!/bin/bash -e\n\n";
-print OUT "echo \" Start compile OpenBlas (will take ~5 min)\n\"\n\n";
+print OUT "echo \" Start compile OpenBlas (will take ~5 min)\"\n\n";
 print OUT "cd $multicom_db_tools_dir/tools\n\n";
 print OUT "cd OpenBLAS\n\n";
 print OUT "make clean\n\n";
@@ -444,7 +444,7 @@ close OUT;
 
 open(OUT,">$install_dir/installation/MULTICOM_manually_install_files/P3_install_freecontact.sh") || die "Failed to open file $install_dir/installation/MULTICOM_manually_install_files/P3_install_freecontact.sh\n";
 print OUT "#!/bin/bash -e\n\n";
-print OUT "echo \" Start compile freecontact (will take ~1 min)\n\"\n\n";
+print OUT "echo \" Start compile freecontact (will take ~1 min)\"\n\n";
 print OUT "cd $multicom_db_tools_dir/tools/DNCON2\n\n";
 print OUT "cd freecontact-1.0.21\n\n";
 print OUT "autoreconf -f -i\n\n";
@@ -473,7 +473,7 @@ close OUT;
 
 open(OUT,">$install_dir/installation/MULTICOM_manually_install_files/P5_python_virtual.sh") || die "Failed to open file $install_dir/installation/MULTICOM_manually_install_files/P5_python_virtual.sh\n";
 print OUT "#!/bin/bash -e\n\n";
-print OUT "echo \" Start install python virtual environment (will take ~1 min)\n\"\n\n";
+print OUT "echo \" Start install python virtual environment (will take ~1 min)\"\n\n";
 print OUT "cd $multicom_db_tools_dir/tools\n\n";
 print OUT "rm -rf python_virtualenv\n\n";
 print OUT "virtualenv python_virtualenv\n\n";
@@ -490,20 +490,27 @@ print OUT "cp $install_dir/installation/MULTICOM_configure_files/keras_multicom.
 close OUT;
 
 
-#### create python2.6 library
+if(!(-e "/usr/bin/python2.6"))
+{
+	#### create python2.6 library
 
-open(OUT,">$install_dir/installation/MULTICOM_manually_install_files/P6_python2.6_library.sh") || die "Failed to open file $install_dir/installation/MULTICOM_manually_install_files/P6_python2.6_library.sh\n";
-print OUT "#!/bin/bash -e\n\n";
-print OUT "echo \" Start install python2.6 library (will take ~5 min)\n\"\n\n";
-print OUT "cd $multicom_db_tools_dir/tools\n\n";
-print OUT "#wget http://www.python.org/ftp/python/2.6.8/Python-2.6.8.tgz\n\n";
-print OUT "#tar xzf Python-2.6.8.tgz\n\n";
-print OUT "cd Python-2.6.8\n\n";
-print OUT "make clean\n\n";
-print OUT "./configure --prefix=$multicom_db_tools_dir/tools/Python-2.6.8 --with-threads --enable-shared --with-zlib=/usr/include\n\n";
-print OUT "make\n\n";
-print OUT "make install\n\n";
-close OUT;
+	open(OUT,">$install_dir/installation/MULTICOM_manually_install_files/P6_python2.6_library.sh") || die "Failed to open file $install_dir/installation/MULTICOM_manually_install_files/P6_python2.6_library.sh\n";
+	print OUT "#!/bin/bash -e\n\n";
+	print OUT "echo \" Start install python2.6 library (will take ~5 min)\"\n\n";
+	print OUT "cd $multicom_db_tools_dir/tools\n\n";
+	print OUT "#wget http://www.python.org/ftp/python/2.6.8/Python-2.6.8.tgz\n\n";
+	print OUT "#tar xzf Python-2.6.8.tgz\n\n";
+	print OUT "cd Python-2.6.8\n\n";
+	print OUT "make clean\n\n";
+	print OUT "./configure --prefix=$multicom_db_tools_dir/tools/Python-2.6.8 --with-threads --enable-shared --with-zlib=/usr/include\n\n";
+	print OUT "make\n\n";
+	print OUT "make install\n\n";
+	close OUT;
+	
+	`cp $install_dir/src/meta/fusioncon/fusion/scripts/Fusion_Abinitio_with_contact.sh.py2.6 $install_dir/src/meta/fusioncon/fusion/scripts/Fusion_Abinitio_with_contact.sh`;
+}else{
+	`cp $install_dir/src/meta/fusioncon/fusion/scripts/Fusion_Abinitio_with_contact.sh.py2.7 $install_dir/src/meta/fusioncon/fusion/scripts/Fusion_Abinitio_with_contact.sh`;
+}
 
 
 
