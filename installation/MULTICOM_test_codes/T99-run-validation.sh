ed79c95081571d4631477f5ddfd978873389f7af
Hou <jh7x3@multicom.eecs.missouri.edu>
2019-06-06 16:49:32 -0500

    Update multicom

---
+++ installation/MULTICOM_test_codes/T99-run-validation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/jh7x3/multicom/test_out/ 
+perl /home/jh7x3/multicom/installation/scripts/validate_predictions.pl  T0993s2  /home/jh7x3/multicom/test_out/ /home/jh7x3/multicom/installation/benchmark
