18624da7e117199f983ea2bb8e8f5ec7993e4c45
Hou <jh7x3@multicom.eecs.missouri.edu>
2019-06-17 23:13:55 -0500

    Update multicom

--- installation/MULTICOM_test_codes/T99-run-validation-integration.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/jh7x3/multicom/test_out/ 
-perl /home/jh7x3/multicom/installation/scripts/validate_integrated_predictions.pl  T1006  /home/jh7x3/multicom/test_out/T1006_multicom/full_length/meta /home/jh7x3/multicom/installation/benchmark/TBM/
--- installation/MULTICOM_test_codes/T99-run-validation.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/jh7x3/multicom/test_out/ 
-perl /home/jh7x3/multicom/installation/scripts/validate_predictions.pl  T1006  /home/jh7x3/multicom/test_out/ /home/jh7x3/multicom/installation/benchmark
